2e78d136c417fe80a50f945a6b77168f17c0ea6c
sk2282001 <92029609+sk2282001@users.noreply.github.com>
2024-04-09 15:04:47 +0530

    Add files via upload

---
+++ Front-End Website/Tour List/script1.js
@@ -0,0 +1,12 @@
+let toggleButton = document.querySelector('.toggle_button');
+let mobileNav = document.querySelector('.mobile__nav');
+let closeBtn = document.querySelector('.btn');
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block'; 
+});
+
+closeBtn.addEventListener('click', function(){
+    mobileNav.style.display = 'none';
+})
+
